a48e053df5a6c1cb73e9f3a55ea7e15a382b24e1
uselogin <uselogin93@gmail.com>
2026-04-18 14:13:36 +0000

    ...

--- four.js
+++ four.js
@@ -1,7 +1,7 @@
-let a="shashi"
-console.log(`${a=="shashi"?"yes":"no"}`)
+let a = "shashi"
+console.log(`${a == "shashi" ? "yes" : "no"}`)
 
-let c=a.split("").reverse().join("");
+let c = a.split("").reverse().join("");
 console.log(c)
 
 const numbers = [1, 2, 2, 3, 4, 4];
@@ -10,3 +10,15 @@ const unique = [...new Set(numbers)];
 
 console.log(unique);
 // [1, 2, 3, 4]
+
+const names = ["Ali", "John", "Ali", "Sara"];
+
+const unique1 = names.reduce((acc, name) => {
+  if (!acc.includes(name)) {
+    acc.push(name);
+  }
+  return acc;
+}, []);
+
+console.log(unique1);
+// ["Ali", "John", "Sara"]
