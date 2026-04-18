febed110ae09ca8222fac1ec7ce5fea03c9fe03a
uselogin <uselogin93@gmail.com>
2026-04-18 06:17:03 +0000

    ..

--- four.js
+++ four.js
@@ -1,2 +1,12 @@
 let a="shashi"
 console.log(`${a=="shashi"?"yes":"no"}`)
+
+let c=a.split("").reverse().join("");
+console.log(c)
+
+const numbers = [1, 2, 2, 3, 4, 4];
+
+const unique = [...new Set(numbers)];
+
+console.log(unique);
+// [1, 2, 3, 4]
