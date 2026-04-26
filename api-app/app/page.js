425938306dd5bb6ae5f8ecd0d25e6e81c9210560
uselogin <uselogin93@gmail.com>
2026-04-26 05:27:21 +0000

    add

--- api-app/app/page.js
+++ api-app/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [email, setemail] = useState()
 
   const handlechange = async () => {
-    const res = await fetch("http://localhost:3000/api/user", {
+    const res = await fetch("https://praction-git-main-uselogins-projects.vercel.app/api/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,7 +22,7 @@ export default function Home() {
     const data = await res.json();
     console.log(data);
   }
-  
+
   return (
     <div className="w-full h-25 flex flex-col items-center justify-center p-5">
         <input type="text" name="userName" id="userName" value={username} onChange={(e) => setuserName(e.target.value)} className="border-2 border-amber-700"/>
