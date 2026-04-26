869a862016c57ebee7da576230c09726941d342f
uselogin <uselogin93@gmail.com>
2026-04-26 05:14:28 +0000

    add

--- api-app/app/page.js
+++ api-app/app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 import { useState,useEffect } from "react";
 
 export default function Home() {
-  const {username, setuserName}=useState()
-  const {password, setpassword}=useState()
-  const {email, setemail}=useState()
+  const [username, setuserName] = useState()
+  const [password, setpassword] = useState()
+  const [email, setemail] = useState()
   return (
     <div>
         <input type="text" name="userName" id="userName" value={username}/>
