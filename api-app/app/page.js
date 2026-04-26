8f66525a3b599cb703da6442ce328fc1272995b5
uselogin <uselogin93@gmail.com>
2026-04-26 05:02:02 +0000

    add

---
+++ api-app/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const {username, setuserName}=useState()
+  const {password, setpassword}=useState()
+  const {email, setemail}=useState()
+  return (
+    <div>
+        <input type="text" name="userName" id="userName" value={username}/>
+        <input type="text" name="password" id="password" value={password}/>
+        <input type="text" name="email" id="email" value={email}/>
+        <button>submit</button>
+    </div>
+  );
+}
